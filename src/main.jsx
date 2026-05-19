beb4a9945ad874b0a951db5de8b5e92b60b99318
francelaelizondo <francelaelizondo5@gmail.com>
2026-05-18 23:35:41 -0600

    Arreglando rutas GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import FavoritesProvider from './context/FavoritesContext'
 
+import FavoritesProvider from './context/FavoritesContext'
 import CartProvider from './context/CartContext'
 
+import { HashRouter } from "react-router-dom"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-  <FavoritesProvider>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </FavoritesProvider>
-</React.StrictMode>
+
+  <React.StrictMode>
+
+    <HashRouter>
+
+      <FavoritesProvider>
+
+        <CartProvider>
+
+          <App />
+
+        </CartProvider>
+
+      </FavoritesProvider>
+
+    </HashRouter>
+
+  </React.StrictMode>
+
 )
